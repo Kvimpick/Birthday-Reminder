06156325c22cda8d96e5f393df7364d104e821f7
Taras Halaida <galaydataras@gmail.com>
2022-07-04 19:34:25 +0300

    Try to understand how React works

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+import List from './List';
+import data from './data';
+
+function App() {
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
+        >
+          clear all
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
